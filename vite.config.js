86765929099ce3a97627362e3981010fb7f8fbd0
pantbros <132388554+pantbros@users.noreply.github.com>
2025-03-10 05:09:15 +0000

    set base url to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/myresume/',
   plugins: [react()],
   server: { port: 3030},
   preview: {
